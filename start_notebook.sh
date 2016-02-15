d0bab897128b7c8acdbfc1cb4d0430d1ea843fd3
Chris Billovits <cjbillov@stanford.edu>
2016-02-14 17:25:03 -0800

    Created environment script for remote machines

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,5 +1,5 @@
 #/bin/bash 
 # Usage: ./start_notebook.sh [virtualenv location] [port]
 
-source ~/${1-deepenv}/bin/activate
+source .env/bin/activate
 jupyter notebook --no-browser --port ${2-16385} 
