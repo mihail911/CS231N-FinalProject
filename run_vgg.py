1749f23dae96199c35de5d3e1ce86374c0dbd568
Chris Billovits <cjbillov@stanford.edu>
2016-02-16 14:06:47 -0800

    Infrastructure changes to vgg19.ipynb (tests 100 image batch from Imagenet validation set).  Modified start_notebooks script to allow for better OpenBLAS performance. Started evolutionary adversarial example module.

--- run_vgg.py
+++ run_vgg.py
@@ -17,73 +17,16 @@ from lasagne.layers.conv import Conv2DLayer as ConvLayer
 from lasagne.layers import MaxPool2DLayer as PoolLayer
 from lasagne.layers import LocalResponseNormalization2DLayer as NormLayer
 from lasagne.utils import floatX
-from models.vgg19 import build_model
-
-
-def prep_image(url, mean_image):
-    
-    ''' Crops and centers an ImageNet image to 3 x 224 x 224. 
-    
-        Return: 
-        :rawim: the cropped image without mean-centering
-        :im: a mean-centered image 
-    '''
-    
-    ext = url.split('.')[-1]
-    im = plt.imread(io.BytesIO(urllib.urlopen(url).read()), ext)
-    # Resize so smallest dim = 256, preserving aspect ratio
-    h, w, _ = im.shape
-    if h < w:
-        im = skimage.transform.resize(im, (256, w*256/h), preserve_range=True)
-    else:
-        im = skimage.transform.resize(im, (h*256/w, 256), preserve_range=True)
-
-    # Central crop to 224x224
-    h, w, _ = im.shape
-    im = im[h//2-112:h//2+112, w//2-112:w//2+112]
-
-    rawim = np.copy(im).astype('uint8')
-
-    # Shuffle axes to c01
-    im = np.swapaxes(np.swapaxes(im, 1, 2), 0, 1)
-
-    # Convert to BGR
-    im = im[::-1, :, :]
-
-    im = im - mean_image[:, None, None]
-    return rawim, floatX(im[np.newaxis])
-
-
-def get_images(num_images=5):
-    index = urllib.urlopen('http://www.image-net.org/challenges/LSVRC/2012/ori_urls/indexval.html').read()
-    image_urls = index.split('<br>')
-
-    np.random.seed(23)
-    np.random.shuffle(image_urls)
-    image_urls = image_urls[:num_images]
-    return image_urls
-
-
-def load_data():
-    with open("vgg19.pkl", "r") as f:
-        model = pickle.load(f)
-
-    classes = model["synset words"]
-    mean_image = model["mean value"]
-    values = model["param values"]
-
-    return classes, mean_image, values
-
+from models import vgg19
 
 if __name__ == "__main__":
-    classes, mean_image, values = load_data()
+    classes, mean_image, values = vgg19.load_data()
 
-    net = build_model()
+    net = vgg19.build_model()
     output_layer = net["prob"]
     lasagne.layers.set_all_param_values(output_layer, values)
 
-    image_urls = get_images(3)
-
+    image_urls = vgg19.get_images(3)
 
     count = 0
     for url in image_urls:
