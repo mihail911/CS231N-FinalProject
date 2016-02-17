9bfb700e335eb42c20f97b843aa414c387594dd4
Mihail Eric <mihail911@gmail.com>
2016-02-16 16:25:38 -0800

    some fixes to image prep function

--- util/util.py
+++ util/util.py
@@ -8,6 +8,7 @@ import skimage.transform
 import sys
 # Janky work around for making sure the module can find 'Lasagne'
 sys.path.append("/Users/mihaileric/Documents/Research/Lasagne")
+import time
 
 from os import listdir
 from os.path import isfile, join
@@ -29,7 +30,7 @@ def get_validation_labels(filename):
     for idx, label in enumerate(labels):
         one_hot_rep[idx, label-1] = 1.
 
-    return one_hot_rep
+    return one_hot_rep, labels
 
 
 def get_image_id_mapping(filename):
@@ -51,6 +52,25 @@ def get_image_id_mapping(filename):
     return id_to_category
 
 
+def scale_image(im):
+    """
+    Scales image to appropriate dimensions
+    :param im:
+    :return:
+    """
+    h, w, _ = im.shape
+    if h < w:
+        im = skimage.transform.resize(im, (256, w*256/h), preserve_range=True)
+    else:
+        im = skimage.transform.resize(im, (h*256/w, 256), preserve_range=True)
+
+    # Central crop to 224x224
+    h, w, _ = im.shape
+    im = im[h//2-112:h//2+112, w//2-112:w//2+112]
+
+    return im
+
+
 def compute_mean_image(data_dir):
     """
     Compute mean image of all images in given dir.
@@ -62,10 +82,20 @@ def compute_mean_image(data_dir):
     # TODO: need to resize images from val set
     for idx, file in enumerate(files):
         img_arr = matplotlib.image.imread(data_dir+"/"+file)
+        if len(img_arr.shape) == 2:
+            h, w = img_arr.shape
+            img_arr = img_arr.reshape(h, w, 1)
+
+        converted_im = scale_image(img_arr)
+
         if idx == 0:
-            mean_image = img_arr
+            mean_image = converted_im
         else:
-            mean_image += img_arr
+            mean_image += converted_im
+
+    mean_image /= len(files)
+
+    return mean_image
 
 
 def prep_image_data(filename, mean_image):
@@ -76,15 +106,7 @@ def prep_image_data(filename, mean_image):
     :return:
     """
     im = matplotlib.image.imread(filename)
-    h, w, _ = im.shape
-    if h < w:
-        im = skimage.transform.resize(im, (256, w*256/h), preserve_range=True)
-    else:
-        im = skimage.transform.resize(im, (h*256/w, 256), preserve_range=True)
-
-    # Central crop to 224x224
-    h, w, _ = im.shape
-    im = im[h//2-112:h//2+112, w//2-112:w//2+112]
+    im = scale_image(im)
 
     rawim = np.copy(im).astype('uint8')
 
@@ -95,17 +117,18 @@ def prep_image_data(filename, mean_image):
     im = im[::-1, :, :]
 
     im = im - mean_image[:, None, None]
-    print rawim
     return rawim, floatX(im[np.newaxis])
 
 
 # Barebones testing code
-    
+
 # get_image_id_mapping("/Users/mihaileric/Documents/CS231N/CS231N-FinalProject/datasets/parsedData.txt")
 # get_validation_labels("/Users/mihaileric/Documents/"
 #                     "CS231N/CS231N-FinalProject/datasets/ILSVRC2014_clsloc_validation_ground_truth.txt")
-#print compute_mean_image("/Users/mihaileric/Documents/CS231N/CS231N-FinalProject/datasets/ILSVRC2012_img_val/")
+# start = time.time()
+# print compute_mean_image("/Users/mihaileric/Documents/CS231N/CS231N-FinalProject/datasets/ILSVRC2012_img_val/")
+# print "Time to compute mean image: ", str(time.time() - start)
 
-#mean_image = np.array([[4], [4], [4]])
-#prep_image_data("/Users/mihaileric/Documents/CS231N/CS231N-FinalProject/"
-                "datasets/ILSVRC2012_img_val/ILSVRC2012_val_00050000.JPEG", mean_image)
+# mean_image = np.array([[4], [4], [4]])
+# prep_image_data("/Users/mihaileric/Documents/CS231N/CS231N-FinalProject/"
+#                 "datasets/ILSVRC2012_img_val/ILSVRC2012_val_00050000.JPEG", mean_image)
