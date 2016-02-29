97efd30bd6ca6941a628d5b079d7d66df1ed4ea9
Mihail Eric <mihail911@gmail.com>
2016-02-28 18:41:01 -0800

    removed a lot of commented out functions in train_pipeline

--- train_pipeline.py
+++ train_pipeline.py
@@ -15,60 +15,6 @@ from util.util import scale_image, get_validation_labels
 from models.vgg19 import build_model
 
 
-
-
-
-# def train_and_predict_funcs(update="nesterov", regularization=0.0):
-#     """
-#     Create theano functions for computing loss, accuracy, etc. for given model
-#     :param model:
-#     :param update: Update parameter to use for training. Select from among
-#                     "nesterov", "sgd", "rmsprop", etc.
-#     :return:
-#     """
-#     input_var = T.tensor4('inputs')
-#     target_var = T.ivector('targets')
-#
-#     model = build_model(input_var)["prob"]
-#
-#     # Create a loss expression for training, i.e., a scalar objective we want
-#     # to minimize (for our multi-class problem, it is the cross-entropy loss):
-#     prediction = lasagne.layers.get_output(model)
-#     loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-#     loss = loss.mean()
-#     # TODO: Add regularization to the loss
-#
-#     params = lasagne.layers.get_all_params(model, trainable=True)
-#     if update == "nesterov":
-#         updates = lasagne.updates.nesterov_momentum(
-#             loss, params, learning_rate=0.01, momentum=0.9)
-#     else:
-#         pass
-#
-#     # Create a loss expression for validation/testing. The crucial difference
-#     # here is that we do a deterministic forward pass through the network,
-#     # disabling dropout layers.
-#     test_prediction = lasagne.layers.get_output(model, deterministic=True)
-#     test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
-#                                                             target_var)
-#     test_loss = test_loss.mean()
-#
-#     test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
-#                       dtype=theano.config.floatX)
-#
-#     # Compile a function performing a training step on a mini-batch (by giving
-#     # the updates dictionary) and returning the corresponding training loss:
-#     train_fn = theano.function([input_var, target_var], loss, updates=updates)
-#
-#     # Compile a second function computing the validation loss and accuracy:
-#     val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
-#
-#     # theano function giving output label for given input
-#     predict_fn = theano.function([input_var, target_var], test_prediction)
-#
-#     return train_fn, val_fn, predict_fn
-
-
 def train(num_epochs=10):
     batch_size = 500
     X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
@@ -121,36 +67,3 @@ def train(num_epochs=10):
     print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
     print("  test accuracy:\t\t{:.2f} %".format(
         test_acc / test_batches * 100))
-
-
-def load_dataset():
-    # NOTE: Need to implement this function in order for compute_accuracy
-    # to work
-    pass
-
-
-# TODO: Refactor this to make use of compute_accuracy_batch
-# def compute_accuracy(model, test=False):
-#     """
-#
-#     :param model: Model to compute accuracy with respect to
-#     :param test:  Whether or not to compute accuracy on test dataset (only use when
-#                     performing final run)
-#     :return: Computed accuracy values
-#     """
-#     _, val_fn, predict_fn = train_and_predict_funcs(model)
-#     X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
-#
-#     _, train_acc = val_fn(X_train, y_train)
-#     _, dev_acc = val_fn(X_val, y_val)
-#
-#     test_acc = None
-#     if test:
-#         _, test_acc = val_fn(X_test, y_test)
-#         print "Train accuracy: {0}, Dev accuracy: {1}, Test accuracy: {2}".\
-#             format(train_acc, dev_acc, test_acc)
-#     else:
-#         print "Train accuracy: {0}, Dev accuracy: {1}".\
-#             format(train_acc, dev_acc)
-#
-#     return train_acc, dev_acc, test_acc
