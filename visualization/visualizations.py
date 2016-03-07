b865c04505f63a582a7b5f732da57700b15c2841
Mihail Eric <mihail911@gmail.com>
2016-03-06 18:49:27 -0800

    code for converting activations to pixel space

--- visualization/visualizations.py
+++ visualization/visualizations.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 import theano
 import theano.tensor as T
@@ -12,9 +13,46 @@ from util.util import show_net_weights, visualize_grid
 def visualize_weights(layer_name, model):
     weights_layer = lasagne.layers.get_all_param_values(model[layer_name])
     filter_weights = weights_layer[-2].transpose(0, 2, 3, 1)
-    #print filter_weights
-    #print filter_weights.shape
     plt.imshow(visualize_grid(filter_weights, padding=1).astype('uint8'))
+    plt.gca().axis('off')
+    plt.gcf().set_size_inches(5, 5)
+    plt.show()
+
+
+def get_activations_at_layer(model, layer_name, input):
+    """
+    Get activations at a given layer of model specified by layer_name
+    :param model:
+    :param layer_name:
+    :param input:
+    :return:
+    """
+    activations = lasagne.layers.get_output(model[layer_name], input)
+    return activations.eval()
+
+
+def convert_to_pixel_space(activations, ubound=255.0):
+    """
+    Convert activations to a visually interpretable representation.
+    :param activations:  Activations to convert to pixel space
+    :param ubound: Image will be scaled to range [0, ubound]
+    :return:
+    """
+    _, filters, H, W = activations.shape
+    rand_filter = np.random.choice(filters)
+
+    print "Rand_filter: ", rand_filter
+
+    filter_activations = activations[0, rand_filter, :, :]
+    low, high = np.min(filter_activations), np.max(filter_activations)
+    converted_img = ubound * (filter_activations - low) / (high - low)
+
+    return converted_img
+
+
+def visualize_img(img):
+    print "img shape: {0}", img.shape
+    plt.imshow(img.astype('uint8'))
     plt.gca().axis('off')
     plt.gcf().set_size_inches(5, 5)
     plt.show()
@@ -25,10 +63,19 @@ if __name__ == "__main__":
     target_var = T.ivector('targets')
 
     _, _, classes, mean_image, values = load_weights("/Users/mihaileric/Documents/CS231N/CS231N-FinalProject/vgg19.pkl")
-    print "Param values dtype:", values[0].dtype
+
+    # Save Idx to Label Mapping for pretrained weights
+    # with open("pretrained_labels.txt", "w") as f:
+    #     for idx, label in enumerate(classes):
+    #         f.write("Label {0}: {1}\n".format(str(idx+1), str(label)))
+
     model = build_model(input_var)
     lasagne.layers.set_all_param_values(model['prob'], values)
 
-    layer_name = "conv4_1"
+    layer_name = "conv1_1"
+    test_input = np.random.randn(1, 3, 224, 224).astype(np.float32)
+    activations = get_activations_at_layer(model, layer_name, test_input)
 
-    #visualize_weights(layer_name, model)
+    converted_img = convert_to_pixel_space(activations)
+    visualize_img(converted_img)
+
